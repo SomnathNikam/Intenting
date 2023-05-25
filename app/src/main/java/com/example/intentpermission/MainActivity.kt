0663ceb947b357c957cb34358ae3458c06a90cb7
SOMNATH <somnathnikam2076@gmail.com>
2023-05-26 02:25:44 +0530

    The Reason

--- app/src/main/java/com/example/intentpermission/MainActivity.kt
+++ app/src/main/java/com/example/intentpermission/MainActivity.kt
@@ -7,5 +7,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //This is O.1 version
     }
 }
